bfcbed5fe83f71a79262ef1deeb68e6ce8ebb309
Jacopo Nasi <jacopo.nasi@gmail.com>
2018-06-12 18:52:56 +0200

    Login working with password hashing

--- 3f3669/index.php
+++ 3f3669/index.php
@@ -4,10 +4,44 @@
 <!--* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
 
 <?php
+    define("BUS_SIZE", "4");
+    define("SQL_HOST", "mysql");
+    define("SQL_USER", "dev");
+    define("SQL_PASS", "dev");
+    define("SQL_DB", "database");
+
+
     if(isset($_GET['user']) && isset($_GET['pass'])) {
-        echo "User: " . $_GET['user'] . ";";
-        echo "Pass: " . $_GET['pass'] . ";";
+        login($_GET['user'], $_GET['pass']);
     }
 
+    function login($user, $pass) {
+        $conn = mysqli_connect(SQL_HOST, SQL_USER, SQL_PASS);
 
+        if (mysqli_connect_errno()) {
+            die("Internal error: connection to DB failed ".
+                mysqli_connect_error());
+        }
+        if (!mysqli_select_db($conn, SQL_DB)) {
+            die("Internal error: selection of DB failed");
+        }
+
+        $sql = "SELECT * FROM Users WHERE user='$user'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+
+                if (password_verify($pass, $row["pass"])) {
+                    echo 'Password is valid!<br><br>Welcome ' . $user . ' <br>';
+                } else {
+                    echo 'Invalid password.<br><br>';
+                }
+
+            }
+        } else {
+            echo "0 results";
+        }
+    }
 ?>
