49df4a2ea13c6691a9a680dfd64aa2a7ef087e69
Jacopo Nasi <jacopo.nasi@gmail.com>
2018-06-13 18:09:37 +0200

    Implementing signup interface with box validation, PHP still missing

--- 3f3669/index.js
+++ 3f3669/index.js
@@ -58,8 +58,77 @@ function login() {
         });
 }
 
+function showSignup() {
+    document.getElementById("login").style.visibility = 'collapse';      // Hide
+    document.getElementById("logged").style.visibility = 'hidden';      // Hide
+
+    document.getElementById("signup_form").style.visibility = 'visible';
+}
+
 function logout() {
     document.cookie = "polixbus_user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "polixbus_hash=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     location.reload();
+}
+
+function signup() {
+    var rU = validateUser();
+    var rP = validatePass();
+
+    if(rU == 1 && rP == 1) {
+
+        var pass = document.getElementById("signup_pass");
+        var user = document.getElementById("signup_user");
+
+        $.post('signup.php', { field1: user, field2 : pass},
+            function(returnedData){
+                console.log(returnedData);
+                location.reload();
+            });
+    }
+}
+
+function validatePass() {
+    var pass = document.getElementById("signup_pass");
+    var repeatpass = document.getElementById("signup_pass-repeat");
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+    var returnValue = -1;
+
+    if(pass.value.match(lowerCaseLetters) && (pass.value.match(upperCaseLetters) || pass.value.match(numbers))) {
+        if(pass.value == repeatpass.value) {
+            pass.style.border = ""
+            repeatpass.style.border = ""
+            returnValue = 1;
+        } else {
+            repeatpass.style.border = "2px solid red"
+            returnValue = 0;
+        }
+
+    } else {
+        repeatpass.style.border = "2px solid red"
+        returnValue = 0;
+    }
+
+    return returnValue;
+
+}
+
+function validateUser() {
+    var user = document.getElementById("signup_user");
+    var regexMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    var returnValue = -1;
+
+    if(regexMail.test(user.value)) {
+        user.style.border = ""
+        returnValue = 1;
+    } else {
+        user.style.border = "2px solid red"
+        returnValue = 0;
+    }
+
+    return returnValue;
 }
---
+++ 3f3669/signup.php
@@ -0,0 +1,45 @@
+<?php
+include 'base.php';
+
+if(isset($_POST['field1']) && isset($_POST['field2'])) {
+    signup($_POST['field1'], $_POST['field2']);
+}
+
+function signup($user, $pass) {
+    $conn = mysqli_connect(SQL_HOST, SQL_USER, SQL_PASS);
+
+    if (mysqli_connect_errno()) {
+        die("Internal error: connection to DB failed ".
+            mysqli_connect_error());
+    }
+    if (!mysqli_select_db($conn, SQL_DB)) {
+        die("Internal error: selection of DB failed");
+    }
+
+//    $sql = "SELECT * FROM Users WHERE user='$user'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+
+            if (password_verify($pass, $row["pass"])) {
+                echo 'Password is valid!<br><br>Welcome ' . $user . ' <br>';
+
+                $cookie_name = 'polixbus_user';
+                $cookie_value = $user;
+                setcookie($cookie_name, $cookie_value, time() + (5*60), '/');
+
+                $cookie_name = 'polixbus_hash';
+                $cookie_value = $row["pass"];
+                setcookie($cookie_name, $cookie_value, time() + (5*60), '/');
+
+            } else {
+                echo 'Invalid password';
+            }
+
+        }
+    } else {
+        echo "USER NOT FOUND";
+    }
+}
